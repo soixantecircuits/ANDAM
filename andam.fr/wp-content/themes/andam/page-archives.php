9213ff21a62b8a09b3fd96e6c98401305b23c065
Emmanuel Geoffray <manuel@soixantecircuits.fr>
2011-12-09 01:05:00 +0100

    first draft of wikipedia API

--- andam.fr/wp-content/themes/andam/page-archives.php
+++ andam.fr/wp-content/themes/andam/page-archives.php
@@ -6,7 +6,4 @@
  */
 get_header(); ?>
 
-<article id="archives" class="grid_16">
-	archive-archives
-</article>			
 <?php get_footer(); ?>
--- andam.fr/wp-content/themes/andam/page-association.php
+++ andam.fr/wp-content/themes/andam/page-association.php
@@ -6,7 +6,4 @@
  */
 get_header(); ?>
 
-<article id="press" class="grid_16">
-	archive-news
-</article>			
 <?php get_footer(); ?>
