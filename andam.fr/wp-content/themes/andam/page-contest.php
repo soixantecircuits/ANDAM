950e9415e423d4ba1fc0cd22fe775968ac464e3f
Michael Fournier <michael@vacuumrandom.com>
2011-12-09 15:23:00 +0100

    page contest/concours

--- andam.fr/wp-content/themes/andam/page-contest.php
+++ andam.fr/wp-content/themes/andam/page-contest.php
@@ -10,6 +10,21 @@ get_header(); ?>
 
 <? echo wpautop($post->post_content); ?>
 
+	<section id="partenaires">
+		<ul>
+			<? 
+				$postid = get_the_ID();
+				$tabinfos = simple_fields_get_post_group_values($postid, "partenaires", true, 2);
+				foreach ($tabinfos as $cle=>$value) {
+					$imageinfos = wp_get_attachment_image_src($value['logo'], 'thumbnail');
+				?>
+				<li><img src="<? echo $imageinfos[0]?>" width="<? echo $imageinfos[1]; ?>" height="<? echo $imageinfos[2]; ?>" /><a><span><? echo $value['nom']; ?></span></a></li>
+					
+				<? };
+			?>
+		</ul>
+	</section>
+
 </article>
 			
 <?php get_footer(); ?>
