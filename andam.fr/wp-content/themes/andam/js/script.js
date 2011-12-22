ef0e0bfce1b85d87a3fee71e40bb99d91c0dd095
Michael Fournier <michael@vacuumrandom.com>
2011-12-22 21:50:03 +0100

    fix css bug

--- andam.fr/wp-content/themes/andam/js/script.js
+++ andam.fr/wp-content/themes/andam/js/script.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
 	if ($('#localbgdurl').length > 0 ) {
 		var bgdurl = $('#localbgdurl img').attr('src');
 		$.backstretch(bgdurl, {speed: 1000});
-	}	
+	}
+	
+	$(".main").fadeIn('slow');	
 });
